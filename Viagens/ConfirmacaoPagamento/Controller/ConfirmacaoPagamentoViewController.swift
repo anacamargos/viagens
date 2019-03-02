921181d2d04f32281a57a06d2072cecdda387b29
Ana <alcvlara@sga.pucminas.br>
2019-03-02 12:13:00 -0300

    Creating new view controller

---
+++ Viagens/ConfirmacaoPagamento/Controller/ConfirmacaoPagamentoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ConfirmacaoPagamentoViewController.swift
+//  Viagens
+//
+//  Created by Ana Leticia Camargos on 02/03/19.
+//  Copyright © 2019 Ana Leticia Camargos. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmacaoPagamentoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
